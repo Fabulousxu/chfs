d264c596169e0433a42b16852840ae97cf701fa4
Fabulousxu <1292248830@qq.com>
2024-12-14 19:26:16 +0800

    finish lab3

--- src/include/rsm/raft/log.h
+++ src/include/rsm/raft/log.h
@@ -5,8 +5,17 @@
 #include <mutex>
 #include <vector>
 #include <cstring>
+#include <unistd.h>
+#include <sys/mman.h>
 
 namespace chfs {
+
+template <typename Command>
+struct RaftLogEntry {
+    int index;
+    int term;
+    Command command;
+};
 
 /** 
  * RaftLog uses a BlockManager to manage the data..
@@ -17,27 +26,90 @@ public:
     RaftLog(std::shared_ptr<BlockManager> bm);
     ~RaftLog();
 
-    /* Lab3: Your code here */
+    void set_state(int current_term, int voted_for);
+    void set_log_entries(std::vector<RaftLogEntry<Command>> &log_entries);
+    void set_snapshot(std::vector<u8> &snapshot);
+    void recover(int &current_term, int &voted_for, std::vector<RaftLogEntry<Command>> &log_entries, std::vector<u8> &snapshot, int &snapshot_index);
 
 private:
     std::shared_ptr<BlockManager> bm_;
     std::mutex mtx;
-    /* Lab3: Your code here */
 
+    int *current_term_ptr;
+    int *voted_for_ptr;
+    int *log_num_ptr;
+    int *snapshot_bytes_ptr;
+    RaftLogEntry<Command> *log_ptr;
+    u8 *snapshot_ptr;
 };
 
 template <typename Command>
-RaftLog<Command>::RaftLog(std::shared_ptr<BlockManager> bm)
+RaftLog<Command>::RaftLog(std::shared_ptr<BlockManager> bm) : bm_(bm)
 {
-    /* Lab3: Your code here */
+    const auto BLOCK_SIZE = bm_->block_size();
+    current_term_ptr = reinterpret_cast<int *>(bm_->unsafe_get_block_ptr());
+    voted_for_ptr = current_term_ptr + 1;
+    log_num_ptr = current_term_ptr + 2;
+    snapshot_bytes_ptr = current_term_ptr + 3;
+    log_ptr = reinterpret_cast<RaftLogEntry<Command> *>(bm_->unsafe_get_block_ptr() + BLOCK_SIZE);
+    snapshot_ptr = reinterpret_cast<u8 *>(bm_->unsafe_get_block_ptr() + BLOCK_SIZE * bm_->total_blocks() / 2);
 }
 
 template <typename Command>
 RaftLog<Command>::~RaftLog()
 {
-    /* Lab3: Your code here */
 }
 
-/* Lab3: Your code here */
+template <typename Command>
+void RaftLog<Command>::set_state(int current_term, int voted_for)
+{
+    std::unique_lock<std::mutex> lock(mtx);
+
+    *current_term_ptr = current_term;
+    *voted_for_ptr = voted_for;
+    
+    msync(current_term_ptr, 2 * sizeof(int), MS_SYNC | MS_INVALIDATE);
+}
+
+template <typename Command>
+void RaftLog<Command>::set_log_entries(std::vector<RaftLogEntry<Command>> &log_entries)
+{
+    std::unique_lock<std::mutex> lock(mtx);
+
+    *log_num_ptr = log_entries.size();
+    for (int i = 0; i < log_entries.size(); i++)
+        log_ptr[i] = log_entries[i];
+    
+    msync(log_num_ptr, sizeof(int), MS_SYNC | MS_INVALIDATE);
+    msync(log_ptr, log_entries.size() * sizeof(RaftLogEntry<Command>), MS_SYNC | MS_INVALIDATE);
+}
+
+template <typename Command>
+void RaftLog<Command>::set_snapshot(std::vector<u8> &snapshot)
+{
+    std::unique_lock<std::mutex> lock(mtx);
+    
+    *snapshot_bytes_ptr = snapshot.size();
+    memcpy(snapshot_ptr, snapshot.data(), snapshot.size());
+    
+    msync(snapshot_bytes_ptr, sizeof(int), MS_SYNC | MS_INVALIDATE);   
+    msync(snapshot_ptr, *snapshot_bytes_ptr, MS_SYNC | MS_INVALIDATE);
+}
+
+template <typename Command>
+void RaftLog<Command>::recover(int &current_term, int &voted_for, std::vector<RaftLogEntry<Command>> &log_entries, std::vector<u8> &snapshot, int &snapshot_index) {
+    std::unique_lock<std::mutex> lock(mtx);
+
+    current_term = *current_term_ptr;
+    voted_for = *voted_for_ptr;
+    snapshot_index = log_ptr[0].index;
+
+    log_entries.clear();
+    for (int i = 0; i < *log_num_ptr; i++)
+        log_entries.push_back(log_ptr[i]);
+
+    snapshot.resize(*snapshot_bytes_ptr);
+    memcpy(snapshot.data(), snapshot_ptr, *snapshot_bytes_ptr);
+}
 
 } /* namespace chfs */
--- src/include/rsm/raft/protocol.h
+++ src/include/rsm/raft/protocol.h
@@ -18,69 +18,136 @@ const std::string RAFT_RPC_APPEND_ENTRY = "append entries";
 const std::string RAFT_RPC_INSTALL_SNAPSHOT = "install snapshot";
 
 struct RequestVoteArgs {
-    /* Lab3: Your code here */
+    int term;
+    int candidate_id;
+    int last_log_index;
+    int last_log_term;
     
     MSGPACK_DEFINE(
-    
+        term,
+        candidate_id,
+        last_log_index,
+        last_log_term
     )
 };
 
 struct RequestVoteReply {
-    /* Lab3: Your code here */
+    int term;
+    bool vote_granted;
 
     MSGPACK_DEFINE(
-    
+        term,
+        vote_granted
     )
 };
 
 template <typename Command>
 struct AppendEntriesArgs {
-    /* Lab3: Your code here */
+    int term;
+    int leader_id;
+    int prev_log_index;
+    int prev_log_term;
+    std::vector<RaftLogEntry<Command>> entries;
+    int leader_commit;
 };
 
 struct RpcAppendEntriesArgs {
-    /* Lab3: Your code here */
+    int term;
+    int leader_id;
+    int prev_log_index;
+    int prev_log_term;
+    std::vector<int> entries_index;
+    std::vector<int> entries_term;
+    std::vector<std::vector<u8>> entries_command;
+    int leader_commit;
 
     MSGPACK_DEFINE(
-    
+        term,
+        leader_id,
+        prev_log_index,
+        prev_log_term,
+        entries_index,
+        entries_term,
+        entries_command,
+        leader_commit
     )
 };
 
 template <typename Command>
 RpcAppendEntriesArgs transform_append_entries_args(const AppendEntriesArgs<Command> &arg)
 {
-    /* Lab3: Your code here */
-    return RpcAppendEntriesArgs();
+    RpcAppendEntriesArgs rpc_arg{
+        arg.term,
+        arg.leader_id,
+        arg.prev_log_index,
+        arg.prev_log_term,
+        {},
+        {},
+        {},
+        arg.leader_commit
+    };
+    for (const auto &[index, term, command] : arg.entries) {
+        rpc_arg.entries_index.push_back(index);
+        rpc_arg.entries_term.push_back(term);
+        rpc_arg.entries_command.push_back(command.serialize(command.size()));
+    }
+    return rpc_arg;
 }
 
 template <typename Command>
 AppendEntriesArgs<Command> transform_rpc_append_entries_args(const RpcAppendEntriesArgs &rpc_arg)
 {
-    /* Lab3: Your code here */
-    return AppendEntriesArgs<Command>();
+    AppendEntriesArgs<Command> arg{
+        rpc_arg.term,
+        rpc_arg.leader_id,
+        rpc_arg.prev_log_index,
+        rpc_arg.prev_log_term,
+        {},
+        rpc_arg.leader_commit
+    };
+    for (int i = 0; i < rpc_arg.entries_index.size(); ++i) {
+        Command command;
+        command.deserialize(rpc_arg.entries_command[i], command.size());
+        arg.entries.push_back({rpc_arg.entries_index[i], rpc_arg.entries_term[i], command});
+    }
+    return arg;
 }
 
 struct AppendEntriesReply {
-    /* Lab3: Your code here */
+    int term;
+    bool success;
 
     MSGPACK_DEFINE(
-    
+        term,
+        success
     )
 };
 
 struct InstallSnapshotArgs {
-    /* Lab3: Your code here */
+    int term;
+    int leader_id;
+    int last_included_index;
+    int last_included_term;
+    int offset;
+    std::vector<u8> data;
+    bool done;
 
     MSGPACK_DEFINE(
-    
+        term,
+        leader_id,
+        last_included_index,
+        last_included_term,
+        offset,
+        data,
+        done
     )
 };
 
 struct InstallSnapshotReply {
-    /* Lab3: Your code here */
+    int term;
 
     MSGPACK_DEFINE(
-    
+        term
     )
 };
 
